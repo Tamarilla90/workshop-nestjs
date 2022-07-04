3dd207d98f49cd04a459195897fb98b06eef1eee
Tamara <tamara.calcerrada@coverwallet.com>
2022-07-04 16:04:23 +0200

    step 7 test

--- src/commons/guard/role.guard.spec.ts
+++ src/commons/guard/role.guard.spec.ts
@@ -1,18 +1,69 @@
 import {RoleGuard} from './role.guard';
 import {Reflector} from "@nestjs/core";
+import {Role} from "./role.enum";
 
 describe('RoleGuard', () => {
 
-  const reflectorMock: Reflector = {
+  let role;
+
+  const reflectorMock = {
     getAllAndOverride: jest.fn(),
     get: jest.fn(),
     getAll: jest.fn(),
     getAllAndMerge: jest.fn()
+  } as jest.Mocked<Reflector>;
+
+  const switchToHttp = {
+    getRequest: jest.fn(),
+  };
+
+  const ctxMock = {
+    switchToHttp: jest.fn().mockImplementation(() => switchToHttp),
+    getHandler: jest.fn(),
+    getClass: jest.fn(),
   }
 
-  it('should be defined', () => {
-    expect(new RoleGuard(reflectorMock)).toBeDefined();
+  beforeAll(() => {
+    role = new RoleGuard(reflectorMock);
   });
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(role).toBeDefined();
+  });
+
+  it('should return true when not exist some component with role', () => {
+    const result = role.canActivate(ctxMock);
+    expect(result).toBeTruthy();
+    expect(reflectorMock.getAllAndOverride).toHaveBeenCalledTimes(1);
+  });
+
+
+  it('should return true when exist some component with role and its valid', () => {
+    reflectorMock.getAllAndOverride.mockReturnValue(Object.values(Role));
+    switchToHttp.getRequest.mockReturnValue({
+      jwt: {
+        scope: ['admin:account']
+      }
+    })
+    const result = role.canActivate(ctxMock);
+    expect(result).toBeTruthy();
+    expect(reflectorMock.getAllAndOverride).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return false when exist some component with role and its not valid', () => {
+    reflectorMock.getAllAndOverride.mockReturnValue(Object.values(Role));
+    switchToHttp.getRequest.mockReturnValue({
+      jwt: {
+        scope: ['admin:users']
+      }
+    })
+    const result = role.canActivate(ctxMock);
+    expect(result).toBeFalsy();
+    expect(reflectorMock.getAllAndOverride).toHaveBeenCalledTimes(1);
+  });
 
 });
--- src/commons/interceptor/transform-response.interceptor.spec.ts
+++ src/commons/interceptor/transform-response.interceptor.spec.ts
@@ -1,7 +1,40 @@
-import { TransformResponseInterceptor } from './transform-response.interceptor';
+import {TransformResponseInterceptor} from './transform-response.interceptor';
+import {catchError, of} from "rxjs";
+import {ExecutionContext, NotFoundException} from "@nestjs/common";
 
 describe('TransformResponseInterceptor', () => {
-  it('should be defined', () => {
-    expect(new TransformResponseInterceptor()).toBeDefined();
-  });
+    let interceptor: TransformResponseInterceptor;
+
+    const nextMock = {
+        handle: jest.fn(),
+    };
+
+    const ctxMock = {
+        getHandler: jest.fn(),
+        getClass: jest.fn(),
+    } as unknown as jest.Mocked<ExecutionContext>;
+
+    beforeAll(() => {
+        interceptor = new TransformResponseInterceptor()
+    });
+
+
+    it('should be defined', () => {
+        expect(interceptor).toBeDefined();
+    });
+
+    it('should send error when no exist data', () => {
+        let error;
+        nextMock.handle.mockReturnValue(of(null));
+        interceptor.intercept(ctxMock, nextMock).pipe(catchError(err => error = err)).subscribe();
+        expect(error).toStrictEqual(new NotFoundException());
+    });
+
+    it('should send data when no exist data', () => {
+        let response;
+        const data = {}
+        nextMock.handle.mockReturnValue(of(data));
+        interceptor.intercept(ctxMock, nextMock).pipe().subscribe((da) => response = da);
+        expect(response).toStrictEqual(data);
+    });
 });
